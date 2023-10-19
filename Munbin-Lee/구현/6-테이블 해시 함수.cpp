f2cf1359e579e08dfed6fe70db4a8b785decdab8
이문빈 <3412mb@gmail.com>
2023-10-19 13:28:22 +0900

    Update 6-테이블 해시 함수.cpp

--- Munbin-Lee/구현/6-테이블 해시 함수.cpp
+++ Munbin-Lee/구현/6-테이블 해시 함수.cpp
@@ -5,16 +5,17 @@
 using namespace std;
 
 int solution(vector<vector<int>> data, int col, int row_begin, int row_end) {
+	col--, row_begin--, row_end--;
     sort(data.begin(), data.end(), [col] (const auto &a, const auto &b) {
-        if (a[col-1] == b[col-1]) return a[0] > b[0];
-        return a[col-1] < b[col-1];
+        if (a[col] == b[col]) return a[0] > b[0];
+        return a[col] < b[col];
     });
     
     int answer = 0;
     for (int i = row_begin; i <= row_end; i++) {
         int s = 0;
-        for (int j : data[i-1]) {
-            s += j % i;
+        for (int j : data[i]) {
+            s += j % (i + 1);
         }
         answer ^= s;
     }
