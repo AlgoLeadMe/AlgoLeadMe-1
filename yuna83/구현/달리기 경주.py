7eda34bd94ad0319b21d5f042bceafe1ddfba17c
Yuna <140380279+yuna83@users.noreply.github.com>
2023-11-01 22:17:47 +0900

    Update 달리기 경주.py

--- yuna83/구현/달리기 경주.py
+++ yuna83/구현/달리기 경주.py
@@ -1,6 +1,12 @@
 def solution(players, callings):
+    player_dict = {player: idx for idx, player in enumerate(players)}
+    
     for call in callings:
-        a = players.index(call)
-        players[a], players[a-1] = players[a-1], players[a]
-       
+        idx = player_dict[call]
+        
+        players[idx], players[idx-1] = players[idx-1], players[idx]
+        
+        player_dict[players[idx]] = idx
+        player_dict[players[idx-1]] = idx-1
+    
     return players
