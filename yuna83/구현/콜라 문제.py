5fc3bfee4b0d1bef1609bd6b94eaddd1fed3d870
Yuna <b1a4coco@gmail.com>
2023-11-03 12:58:18 +0900

    2023.11.03

---
+++ yuna83/구현/콜라 문제.py
@@ -0,0 +1,9 @@
+def solution(a, b, n):
+    answer = 0
+    while (n>=a):
+        remain_bottle = n%a
+        n = (n//a)*b
+        answer += n
+        n += remain_bottle
+       
+    return answer
