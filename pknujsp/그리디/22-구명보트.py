d518cab52e9fdc182f4c4a9c84be09a435129ebd
JSPark <48265129+pknujsp@users.noreply.github.com>
2023-11-26 21:03:06 +0900

    2023-11-26

--- pknujsp/그리디/22-구명보트.py
+++ pknujsp/그리디/22-구명보트.py
@@ -1,19 +1,39 @@
+# deque
+
 from collections import *
+
 
 def solution(people, limit):
     people = deque(sorted(people, reverse=True))
-    
+
     count = 0
-    weight = people.popleft()
-    
     while people:
-        if weight + people[-1] <= limit:
+        if people[0] + people[-1] <= limit:
             people.pop()
-            
-        weight = people.popleft() if people else 0
+
+        if people:
+            people.popleft()
+
         count += 1
-    
-    if weight:
+
+    return count
+
+
+# two pointer
+
+
+def solution(people, limit):
+    people.sort(reverse=True)
+
+    count = 0
+    heavy_idx = 0
+    light_idx = len(people) - 1
+
+    while heavy_idx <= light_idx:
+        if people[heavy_idx] + people[light_idx] <= limit:
+            light_idx -= 1
+
+        heavy_idx += 1
         count += 1
-        
+
     return count
