e3bbd9e59d20c6215c700866c43dc7de170983dc
JSPark <48265129+pknujsp@users.noreply.github.com>
2023-11-26 19:28:16 +0900

    2023-11-26

--- pknujsp/그리디/22-구명보트.py
+++ pknujsp/그리디/22-구명보트.py
@@ -4,16 +4,16 @@ def solution(people, limit):
     people = deque(sorted(people, reverse=True))
     
     count = 0
-    weight_sum = people.popleft()
+    weight = people.popleft()
     
     while people:
-        if weight_sum + people[-1] <= limit:
+        if weight + people[-1] <= limit:
             people.pop()
             
-        weight_sum = people.popleft() if people else 0
+        weight = people.popleft() if people else 0
         count += 1
     
-    if weight_sum:
+    if weight:
         count += 1
         
     return count
