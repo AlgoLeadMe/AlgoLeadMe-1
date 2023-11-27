6c56a5d15c1b418fd786f20adc23dd11a982dd91
이문빈 <3412mb@gmail.com>
2023-11-27 22:58:09 +0900

    2023-11-27 최솟값 찾기

---
+++ Munbin-Lee/슬라이딩윈도우/21-최솟값 찾기.py
@@ -0,0 +1,15 @@
+from collections import deque
+
+stdin = open(0)
+N, L = map(int, stdin.readline().split())
+A = list(map(int, stdin.readline().split()))
+deq = deque()
+answer = []
+
+for k, v in enumerate(A):
+    while deq and deq[-1][1] >= int(v): deq.pop()
+    if deq and deq[0][0] + L <= k: deq.popleft()
+    deq.append([k, v])
+    answer.append(deq[0][1])
+
+print(' '.join(map(str, answer)))
