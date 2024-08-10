9c598c75ed2ffe30a3e50afcbcc0148e1619af9b
H0ngJu <hongju4385@gmail.com>
2024-08-10 21:08:04 +0900

    2024-08-10

--- H0ngJu/베스트앨범.py
+++ H0ngJu/베스트앨범.py
@@ -1,6 +1,7 @@
-genres = ["classic", "pop", "classic", "classic", "pop"]
-plays = [500, 600, 150, 800, 2500]
+genres = ["classic", "pop", "classic", "classic", "pop", "ex", "ex"]
+plays = [500, 600, 150, 800, 2500, 900, 400]
 answer = []
+f = 0
 
 dic = {}
 info = {}
@@ -19,15 +20,15 @@ for i in range(len(genres)):
 best_g = sorted(dic.items(), key=lambda x: x[1], reverse=True)
 
 for g, _ in best_g:
+    if f == 2: break
     best_album = [(plays[i], i) for i in range(len(genres)) if genres[i] == g]
     for i in range(len(best_album)):
         for j in range(i+1, len(best_album)):
             if best_album[j][0] > best_album[i][0]: #재생수가 작은 경우
                 best_album[i], best_album[j] = best_album[j], best_album[i]
-            elif best_album[j][0] == best_album[i][0] and best_album[j][1] < best_album[i][1]: #idx가 작은 경우
-                best_album[i], best_album[j] = best_album[j], best_album[i]
     
     answer.extend([x[1] for x in best_album[:2]])
+    f += 1
 
 
 print(answer)
