4a7e5b03bd425c225655d0efc04a8724bf14431b
H0ngJu <hongju4385@gmail.com>
2024-11-23 12:21:02 +0900

    2024-11-23

--- H0ngJu/DP/전깃줄.py
+++ H0ngJu/DP/전깃줄.py
@@ -3,7 +3,6 @@ import sys
 def input() : return sys.stdin.readline().rstrip()
 
 N = int(input())
-# N <= 100 A <= 500 B <= 500 -> 완탐으로 하면 역시 시간초과
 info = [list(map(int, input().split())) for _ in range(N)]
 
 info.sort()
